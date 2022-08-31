92c4c753110d57cafee3d80efb7aae2e0842640e
EugeneKlyuenkov <dawd>
2022-08-31 10:35:38 +0300

    initial setup

---
+++ Podfile
@@ -0,0 +1,18 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '11.0'
+
+use_frameworks!
+
+workspace 'VideoCamera'
+
+target 'VideoCamera_Framework' do
+    project 'VideoCamera_Framework.xcodeproj'
+    pod 'Shakuro.CommonTypes', '1.1.4'
+end
+
+target 'VideoCamera_Example' do
+    project 'VideoCamera_Example.xcodeproj'
+    pod 'Shakuro.CommonTypes', '1.1.4'
+    pod 'SwiftLint', '0.43.1'
+end
---
+++ VideoCamera_Example/Example.swift
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2018-2020 Shakuro (https://shakuro.com/)
+// Sergey Laschuk
+//
+
+import UIKit
+
+internal struct Example {
+
+    internal let title: String
+    private let viewControllerType: (UIViewController & ExampleViewControllerProtocol).Type
+
+    internal init(title aTitle: String, viewControllerType aViewControllerType: (UIViewController & ExampleViewControllerProtocol).Type) {
+        title = aTitle
+        viewControllerType = aViewControllerType
+    }
+
+    internal func instantiateViewController() -> UIViewController {
+        return viewControllerType.instantiate(example: self)
+    }
+
+}
+
+internal extension Example {
+
+    static func all() -> [Example] {
+        return [
+            Example(title: "ImageProcessor", viewControllerType: ExampleImageProcessorViewController.self),
+            Example(title: "VideoCamera", viewControllerType: ExampleVideoCameraViewController.self)
+        ]
+    }
+
+}
